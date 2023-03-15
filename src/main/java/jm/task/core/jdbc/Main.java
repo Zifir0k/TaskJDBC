4edc4a323d1c987ef0282bc4705eb68802aefea2
Zifir0k <evgenizef.ak@gmail.com>
2023-03-15 18:57:24 +1000

    commend

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.cleanUsersTable();
+
+        userService.saveUser("Andje", "Sapkovski", (byte) 74);
+
+        userService.saveUser("Oliver", "Bolden", (byte) 75);
+
+        userService.saveUser("Govard", "Lavkraft", (byte) 59);
+
+        userService.saveUser("Maria", "Remark", (byte) 74);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
